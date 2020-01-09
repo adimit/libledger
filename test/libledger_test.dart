3c70f4e82e815ac441ff27117677bfa88711001f
Aleksandar Dimitrov <git@aleks.bg>
2020-01-09 20:05:49 +0100

    Add more tests

--- test/libledger_test.dart
+++ test/libledger_test.dart
@@ -6,24 +6,65 @@ void castAndCheck<T>(dynamic x, void Function(T) f) {
   f(x as T);
 }
 
+// When we don't need an assertion, because we just want a successful parse
+void noop(List<Transaction> transactions) {
+  expect(true, isTrue);
+}
+
+void parseTest(String description, String subject,
+    [void Function(List<Transaction>) assertions = noop]) {
+  test('Parses $description', () {
+    castAndCheck<ParseSuccess>(parseTransactions(subject), (result) {
+      assertions(result.transactions);
+    });
+  });
+}
+
 void main() {
   group('Parser', () {
     setUp(() {});
 
-    test('Parses empty string as no transactions', () {
-      castAndCheck<ParseSuccess>(parseTransactions(''), (result) {
-        expect(result.transactions, isEmpty);
-      });
+    parseTest('empty string as no transactions', '', (transactions) {
+      expect(transactions, isEmpty);
     });
 
-    test('Incremental test for parsing', () {
-      castAndCheck<ParseSuccess>(parseTransactions('''2010/10/01 description foo bar ä
-          foobar  '''),
-          (result) {
-        expect(result.transactions.length, equals(1));
-        expect(result.transactions.first.date.date1, equals('2010/10/01'));
-        expect(result.transactions.first.description, equals('description foo bar ä'));
-      });
+    parseTest('transaction declaration without any transfers',
+        '2020/01/09 this is a description', (transactions) {
+      expect(transactions.length, equals(1));
+      expect(transactions.first.date, equals('2020/01/09'));
+      expect(transactions.first.description, equals('this is a description'));
     });
+
+    parseTest('transaction declaration with excess whitespace',
+        '2020/01/09           this is a description            ',
+        (transactions) {
+      expect(transactions.first.description, equals('this is a description'));
+      expect(transactions.first.date, equals('2020/01/09'));
+    });
+    parseTest(
+        'transfer with account, but without amount', '''2020/01/09 description
+        Account:Number1''');
+    parseTest('transfer with account and amount', '''2020/01/09 description
+        Account:Number1  20 EUR''');
+    parseTest('transfer with account and amount more whitespace',
+        '''2020/01/09 description
+        Account:Number1              20 EUR''');
+    parseTest('transfer with two accounts', '''2020/01/09 description
+        Account:Number1  20 EUR
+        Account:Number2  -20 EUR
+        ''');
+    parseTest('transfer with two accounts, and only one amount',
+        '''2020/01/09 description
+        Account:Number1  20 EUR
+        Account:Number2
+        ''');
+    parseTest('extraneous whitespaces around transaction', '''
+
+        2020/01/09     description     
+        Account:Number1           20 EUR        
+        Account:Number2         
+
+
+        ''');
   });
 }
