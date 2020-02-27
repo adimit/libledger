2be8cf58f83fe93715e6139d27fae6a85ff39f13
Aleksandar Dimitrov <git@aleks.bg>
2020-02-27 20:54:48 +0100

    Factor out test utils

--- test/libledger_test.dart
+++ test/libledger_test.dart
@@ -3,34 +3,7 @@ import 'package:petitparser/petitparser.dart';
 import '../lib/src/grammar.dart';
 import '../lib/src/semantics.dart';
 import 'package:test/test.dart';
-
-void castAndCheck<T>(dynamic x, void Function(T) f) {
-  expect(x, isA<T>(), reason: 'Expected ${T} but got ${x.toString()}');
-  f(x as T);
-}
-
-// When we don't need an assertion, because we just want a successful parse
-void noop(dynamic anyArgument) {
-  expect(true, isTrue);
-}
-
-void parseSuccess<T extends Statement>(String description, String subject,
-    [void Function(List<T>) assertions = noop]) {
-  test('parses $description', () {
-    castAndCheck<ParseSuccess>(parseTransactions(subject), (result) {
-      assertions(result.transactions.cast<T>());
-    });
-  });
-}
-
-void parseFailure(String description, String subject,
-    [void Function(ParseError error) assertions = noop]) {
-  test('Does not parse $description', () {
-    castAndCheck<ParseError>(parseTransactions(subject), (result) {
-      assertions(result);
-    });
-  });
-}
+import 'test_util.dart';
 
 void main() {
   group('Parser', () {
