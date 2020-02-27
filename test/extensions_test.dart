a1d9520aa8718c2e8dfa20398681f13d15b568e8
Aleksandar Dimitrov <git@aleks.bg>
2020-02-27 20:55:03 +0100

    Add first expectation

--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -1,12 +1,23 @@
 import 'package:test/test.dart';
 import '../lib/src/grammar.dart';
+import 'package:petitparser/petitparser.dart';
 
 void main() {
-  group('necessarilyseparatedby', () {
-    test('a,a without seps', () {});
-    test('a,a with seps', () {});
-    test('a,a, without trailing flag', () {});
-    test('a,a, with trailing flag', () {});
-    test('a,b failure', () {});
+  group('extensions', () {
+    group('necessarilyseparatedby', () {
+      test('a,a without seps', () {
+        final result = char('a')
+            .necessarilySeparatedBy(char(','), includeSeparators: false)
+            .parse('a,a');
+
+        expect(result, isA<Success>());
+        expect((result as Success).value, equals(['a', 'a']));
+      });
+      test('just a fails', () {});
+      test('a,a with seps', () {});
+      test('a,a, without trailing flag', () {});
+      test('a,a, with trailing flag', () {});
+      test('a,b failure', () {});
+    });
   });
 }
