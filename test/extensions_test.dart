00cf7b8c8aa5d42df3a5eaad471465b61b9ea86b
Aleksandar Dimitrov <git@aleks.bg>
2020-02-27 20:58:25 +0100

    Introduce castAndCheck

--- test/extensions_test.dart
+++ test/extensions_test.dart
@@ -1,20 +1,28 @@
 import 'package:test/test.dart';
 import '../lib/src/grammar.dart';
 import 'package:petitparser/petitparser.dart';
+import 'test_util.dart';
 
 void main() {
   group('extensions', () {
     group('necessarilyseparatedby', () {
       test('a,a without seps', () {
-        final result = char('a')
-            .necessarilySeparatedBy(char(','), includeSeparators: false)
-            .parse('a,a');
-
-        expect(result, isA<Success>());
-        expect((result as Success).value, equals(['a', 'a']));
+        castAndCheck<Success>(
+            char('a')
+                .necessarilySeparatedBy(char(','), includeSeparators: false)
+                .parse('a,a'), (result) {
+          expect(result.value, equals(['a', 'a']));
+        });
       });
       test('just a fails', () {});
-      test('a,a with seps', () {});
+      test('a,a with seps', () {
+        castAndCheck<Success>(
+            char('a')
+                .necessarilySeparatedBy(char(','), includeSeparators: true)
+                .parse('a,a'), (result) {
+          expect(result.value, equals(['a', ',', 'a']));
+        });
+      });
       test('a,a, without trailing flag', () {});
       test('a,a, with trailing flag', () {});
       test('a,b failure', () {});
